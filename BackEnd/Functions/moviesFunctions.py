7ad3539e07fc44a7a3af6d1d088d3444c964b931
DiegoCamacho08 <diego.camacho@linkthinks.com>
2024-07-05 14:23:02 -0600

    .

---
+++ BackEnd/Functions/moviesFunctions.py
@@ -0,0 +1,9 @@
+import jet
+import BackEnd.GlobalInfo.Helpers as Helpers
+import BackEnd.GlobalInfo.ResponseMessages as ResponseMessages
+
+from BackEnd.GlobalInfo.Keys import jwtKey, webUrl
+from bson.objectid import ObjectId
+
+dbConnLocal = Helpers.dbConnection()
+
